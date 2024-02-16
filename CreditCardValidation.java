cd0944a464eaa3a2af72f093d06019ed22223a64
Ali <36553593+aad3m@users.noreply.github.com>
2024-02-15 23:44:28 -0600

    Main

---
+++ CreditCardValidation.java
@@ -0,0 +1,56 @@
+import java.util.Scanner;
+
+public class CreditCardValidation {
+    public static void main(String[] args){
+        System.out.println("Welcome to Credit Card Validator");
+
+        // Get user input
+        Scanner scrn = new Scanner(System.in);
+        System.out.print("Enter an 8-digit number: ");
+        String userinput = scrn.nextLine();
+
+        // Validate user input
+        if (userinput.length() != 8){
+            System.out.println("That's not an 8-digit number!! ");
+            return;
+        }
+
+        // Calculate double digit sum
+        int ddsum = 0;
+        for (int i = 0; i < userinput.length(); i += 2) {
+            char c = userinput.charAt(i);
+            int n = Character.getNumericValue(c);
+            n *= 2;
+            if (n >= 10) {
+                ddsum += (n % 10) + (n / 10);
+            }
+            else {
+                ddsum += n;
+            }
+
+        }
+        System.out.println(ddsum);
+
+        // Calculate remaining digits sum
+        int rdsum = 0;
+        for (int i = 1; i < userinput.length(); i += 2) {
+            char c = userinput.charAt(i);
+            int n = Character.getNumericValue(c);
+            rdsum += n;
+        }
+        System.out.println(rdsum);
+
+
+        // Validate credit card
+        int total = ddsum + rdsum;
+        if (total % 10 == 0) {
+            System.out.println("Valid credit card number"); }
+        else {
+            System.out.println("Invalid credit card number");
+        }
+
+
+
+
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # Credit Card Validation
- 
+- Last Digit of a Credit Card Number is The Check Digit
+- If The Last Digit of The Result is 0 Card is Valid
+- Must Be Length Specified
+
+_**Valid Number Example:**
+
+<img alt="img.png" src="img.png" width="225"/>
+
+**_Invalid Number Example:_**
+
+<img alt="img_1.png" src="img_1.png" width="225"/>
+
+**_Invalid Length Example:_**
+
+<img alt="img_2.png" src="img_2.png" width="225"/>
